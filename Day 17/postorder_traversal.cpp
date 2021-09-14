d5d9fbc56d592c350c306f7f91939c5aa587d53a
hey-shashikant <shashikantsolanki041@gmail.com>
2021-09-14 22:41:43 +0530

    Day 17

---
+++ Day 17/postorder_traversal.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node* left;
+	node* right;
+	node(int x)
+	{
+		data = x;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+void PostOrder(node* root)
+{
+	if(root == nullptr)
+		return;
+	PostOrder(root->left);
+	PostOrder(root->right);
+	cout << root->data << " ";
+}
+
+/*
+void IterativePostorder(node* root)
+{
+	stack<node*>stk;
+	while(stk.size() > 0 or root != nullptr)
+	{
+		if(root != nullptr)
+		{
+			stk.push(root);
+			cout << root->data << " ";
+			root = root->left;
+		}
+		else
+		{
+			node* t = stk.top();
+			stk.pop();
+			root = t->right;
+		}
+	}
+}
+*/
+
+int main()
+{
+	int x;
+	cout << "\n Enter root : ";
+	cin >> x;
+	node* root = new node(x);
+	queue<node*>q;
+	q.push(root);
+	while(!q.empty())
+	{
+		node* t = q.front();
+		q.pop();
+		int l,r;
+		cout << "\n Enter left child of " << t->data << " : ";
+		cin >> l;
+		if(l != -1)
+		{
+			node* temp = new node(l);
+			t->left = temp;
+			q.push(temp);
+		}
+		cout << "\n Enter right child of " << t->data << " : ";
+		cin >> r;
+		if(r != -1)
+		{
+			node* temp = new node(r);
+			t->right = temp;
+			q.push(temp);
+		}
+	}
+	cout << "\n Post-order traversal : ";
+	PostOrder(root);
+	//cout << "\n Iterative Post-order traversal : ";
+	//IterativePostorder(root);
+	return 0;
+}
